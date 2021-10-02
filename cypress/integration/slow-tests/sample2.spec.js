df0921176868881029928264d34799ac253f186c
Matheus Santos <62854319+MatheusSantosz@users.noreply.github.com>
2021-10-02 20:40:15 -0300

    update should('to.be.empty')
    validando se o campo esta limpo!

--- cypress/integration/slow-tests/sample2.spec.js
+++ cypress/integration/slow-tests/sample2.spec.js
@@ -3,9 +3,9 @@ describe('Slow tests bad practice - Sample 2', () => {
     cy.visit('https://notes-serverless-app.com/signup')
 
 
-    cy.get('#email').type('joe@example.com')
-    cy.get('#password').type('foobarbaz')
-    cy.get('#confirmPassword').type('foobahbaz')
+    cy.get('#email').should('to.be.empty').type('joe@example.com')
+    cy.get('#password').should('to.be.empty').type('foobarbaz')
+    cy.get('#confirmPassword').should('to.be.empty').type('foobahbaz')
 
     cy.contains('button', 'Signup')
       .should('be.disabled')
